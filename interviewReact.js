602d962805f41120ad3e56021d5cef27ba481aa8
Atik <98823300+Atik074@users.noreply.github.com>
2023-12-19 19:49:33 +0600

    Update interviewReact.js

--- interviewReact.js
+++ interviewReact.js
@@ -1,1 +1,50 @@
 console.log("interview question is added readme file in this repo")
+                       Bootcamp one 
+                 subject : React basic concepts 
+
+00.what is react ? 
+Ans: React is an efficient, declarative, and flexible open-source JavaScript library for building simple, fast, and scalable frontends of web application.
+
+01. Differentiate between Real DOM and Virtual DOM.?  
+      Ans: :Real dom is the  browser’s  representation of  HTML   structures.when user  interacts with website, real dom reflects it and updated html.
+
+ Virtual  dom is an abstraction of real dom and lightweight copy of real dom.when user change any state,virtual dom updates only changable part by diffing algorithm .it has another name reconsilation..
+
+
+Real dom : every  update requires the browser to recalculate the entire document layout.virtual dom allows for faster updates and improved performance.
+	
+Virtual DOM is highly flexible and can be used with any programming language. Real DOM is tightly coupled with the browser and can only be manipulated using JavaScript.
+
+02. List some of the major advantages of React  
+Ans:
+i)	 Flexibility due to modular structure
+ii)	   Complex app can run high performance for virtual Dom and server side rendering 
+iii)	 to learn react you need have basic knowledge of javascript
+iv)	Deploying React is fairly easy to accomplish if you have some basic knowledge of JavaScript.
+v)	It has reuseable components that saves time  and removes same code repitation 
+
+03)    Limitation of React js 
+i)	Lack of proper documentation
+ii)	Developer speed 
+iii)	Jsx complexity
+iv)	Problem with SEO
+v)	Steep learnig curve: without good knowledge of javascript you can not understand properly react.Virtual dom, jsx syntax, state management of react is painfull,harder concenpt for new  developer
+vi)	Boilerplate code: to start new project you need to significant amount of set up and need configuration.set up project structure,installing dependencies,configuration tools such as babel, webpack are count in Boilerplate code.
+     04) Why React is best choice ? 
+   Ans: 
+i)	Easy to learn 
+ii)	It helps to build user interface
+iii)	Allows writing custome code
+iv)	Fast rendering
+v)	SEO friendly
+vi)	Strong community support
+vii)	Available extension
+
+
+
+
+
+
+
+
+
